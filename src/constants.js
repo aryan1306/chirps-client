133ab97fa04dac1b938ca715a98a5dfefbd28b99
Aryan <work.aryan.singhal@gmail.com>
2022-05-21 22:24:39 +0530

    add prod api url with https

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-// export const API_URL = __prod__
-// 	? "http://44.192.107.2:8080"
-// 	: "http://localhost:8080";
-export const API_URL = "http://3.215.79.64:8080";
+export const API_URL = __prod__
+	? "https://aryansinghal.xyz"
+	: "http://localhost:8080";
